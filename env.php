d5966a7abbb2a6b2d385df4960985f2e680bee90
rezki wissem <wissemrezki@gmail.com>
2022-12-20 13:07:58 +0100

    php todolistv3

--- env.php
+++ env.php
@@ -1,5 +1,5 @@
 <?php
-putenv("MYSQL_HOST=bbr6qriktr5g2vwk9xfio-mysql.services.clever-cloud.com");
-putenv("MYSQL_DATABASE=bbr6qriktr5g2vwk9xfio");
+putenv("MYSQL_HOST=br6qriktr5g2vwk9xfio-mysql.services.clever-cloud.com");
+putenv("MYSQL_DATABASE=br6qriktr5g2vwk9xfio");
 putenv("MYSQL_USER=uwwvwgpwo7oba3un");
 putenv("MYSQL_PASS=5Co8ODjTIEgztMzodjF3");
